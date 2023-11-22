ac5e8c8e74e6f0368d0e17a3e26b7c235f6e1494
Nelson Ortiz Leal <114643735+NelsonOrtizLeal@users.noreply.github.com>
2023-11-21 18:03:14 -0600

    Utilizando funcion ToWords

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
+Console.WriteLine("¿Cual es tu nombre?");
+var name = Console.ReadLine();
+
+Console.WriteLine("¿Cual es su cargo?");
+var job = Console.ReadLine();
+
+Console.WriteLine("¿Cuantos años tienes?");
+var input = Console.ReadLine();
+int age;
+
+if (!Int32.TryParse(input, out age))
+{
+    age = 18;
+}
+
+if (age > 0)
+{
+    Console.WriteLine($"Hola, mi nombre es {name}, mi cargo es {job} y tengo {age.ToWords(new System.Globalization.CultureInfo("es"))} años");
+}else{
+    Console.WriteLine("La edad no puede ser negativa o 0");
+}
